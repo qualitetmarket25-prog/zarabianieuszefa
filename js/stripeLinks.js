93843e478a73164faddbdb5de3ffd19757f6bb0e
qualitetmarket25-prog <qualitetmarket25@gmail.com>
2026-03-07 00:26:15 +0100

    Create stripeLinks.js

---
+++ js/stripeLinks.js
@@ -0,0 +1,11 @@
+// js/stripeLinks.js
+
+window.QM_STRIPE = {
+
+  BASIC: "https://buy.stripe.com/test_eVq5kF1uZdRk7QXcLQ5J603",
+
+  PRO: "https://buy.stripe.com/test_bJe4gBc9D7sW0ov7rw5J606",
+
+  ELITE: "https://buy.stripe.com/test_28E4gBddHcNg9Z51385J605"
+
+};
