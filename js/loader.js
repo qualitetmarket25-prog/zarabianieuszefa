faf04bc9485b154103b72ca63dd513126d8e7ebb
qualitetmarket25-prog <qualitetmarket25@gmail.com>
2026-03-07 01:57:07 +0100

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,25 +1,18 @@
 // QUALITET LOADER SAFE
 
 (function () {
-
   const files = [
-
     "js/utils-storage.js",
     "js/csv-import.js",
     "js/products-engine.js",
-    "js/panel-store.js"
-
+    "js/panel-store.js",
+    "js/shop-render.js"
   ];
 
-  files.forEach(src => {
-
+  files.forEach(function (src) {
     const s = document.createElement("script");
-
     s.src = src;
     s.defer = true;
-
     document.body.appendChild(s);
-
   });
-
 })();
